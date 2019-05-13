6d97e80b22abdce1fbd557eb789f3e8de9207eb2
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2019-05-12 22:45:42 -0500

    firebase

--- app/src/main/java/findspot/almac/myapplication/LoginActivity.java
+++ app/src/main/java/findspot/almac/myapplication/LoginActivity.java
@@ -2,12 +2,29 @@ package findspot.almac.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText et_usuario;
+    private EditText et_contrasenha;
+
+    private Button btn_ingresar;
+    private Button btn_registrate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_usuario = findViewById(R.id.et_usuario);
+        et_contrasenha = findViewById(R.id.et_contraseña);
+
+        btn_ingresar = findViewById(R.id.btn_ingresar);
+        btn_registrate = findViewById(R.id.btn_registrarte);
+
+
+
     }
 }
